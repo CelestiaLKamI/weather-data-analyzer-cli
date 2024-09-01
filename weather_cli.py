8cf628e8ce9fca74b4fea894a92671b387e3d96a
CelestiaLKamI <work.mayukh14@gmail.com>
2024-09-01 12:23:23 +0530

    updated commit

--- weather_cli.py
+++ weather_cli.py
@@ -3,5 +3,30 @@ import json
 import sys
 
 
-def load_data(Weather_data):
-    
+def load_data(weather_data):
+    with open(weather_data, 'r') as file:
+        data = json.load(file)
+
+    return data
+
+
+def calculate_avg_temp(data):
+    cities = {}
+    for each in data:
+        city = each["city"]
+        temperature = each["temperature"]
+        
+        if city not in cities:
+            cities[city] = []
+        
+            cities[city].append(temperature)
+
+
+    avg_temp = {} 
+
+    for city, temps in cities.items():
+            
+            average_temp = sum(temps) / len(temps)
+            avg_temp[city] = average_temp
+
+    return avg_temp
