27e6755dcc65ed60321919fbcd6b6df3eacca95a
CelestiaLKamI <work.mayukh14@gmail.com>
2024-09-01 14:49:55 +0530

    function changes and cli script updates

--- weather_cli.py
+++ weather_cli.py
@@ -10,9 +10,9 @@ def load_data(weather_data):
     return data
 
 
-def calculate_avg_temp(data):
+def calculate_avg_temp(temp_list):
     cities = {}
-    for each in data:
+    for each in temp_list:
         city = each["city"]
         temperature = each["temperature"]
         
@@ -30,3 +30,42 @@ def calculate_avg_temp(data):
             avg_temp[city] = average_temp
 
     return avg_temp
+
+def convert_temp(avg_temp, convert = None):
+
+    for city in avg_temp:
+            avg_temp[city] = (avg_temp[city] * 9/5) + 32
+
+    for city, temp in avg_temp.items():
+
+        if convert:
+            temp = convert_temp(temp)
+            unit = 'F'
+        else:
+            unit = 'C'
+        return f"{city}: {temp} degree {unit}"
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python weather_cli.py [--city CITY] [--convert F] [--list]")
+        sys.exit(1)
+
+    file_path = "./weather_data.json"
+    action = sys.argv[1]
+
+    if action not in ["--city", "--convert", "--list", "--help"]:
+        print("ERROR: Please specify one action")
+        print("--city", "--convert", "--list", "--help")
+        sys.exit(1)
+
+    if action == "--city":
+        print("Please Specify the city: ")
+        city = sys.argv[2]
+    
+    elif action == "--convert":
+        print
+
+
+
+
